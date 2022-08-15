3ebda3b999915c4d2820cce2ab2368df27b7c928
ITBrick-TatyanaK <swastgirl@gmail.com>
2022-08-15 18:12:52 +0300

    123

--- src/test/java/sortComporator/Test.java
+++ src/test/java/sortComporator/Test.java
@@ -1,11 +1,13 @@
 package sortComporator;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
+
         List<Car> list = new ArrayList<>();
         list.add(new Car(1954,"Volga"));
         list.add(new Car(1992,"Volkswagen"));
@@ -15,12 +17,29 @@ public class Test {
         list.add(new Car(2020,"Reno"));
 
         Collections.sort(list,new SortByYear());
-        Collections.sort(list,new SortByModel());
-
         for(Car car: list){
             System.out.println(car.getModel()+ " | " +car.getYear());
         }
+        System.out.println("===========");
+
+        Collections.sort(list,new SortByModel());
+        for(Car car: list){
+            System.out.println(car.getModel()+ " | " +car.getYear());
+        }
+
+
+        ////
+        Car car1 = new Car(100,"Фмодель");
+        Car car2 = new Car(50,"Бмодель");
+        Car car3 = new Car(67,"Жмодель");
+        Car car4 = new Car(3,"Амодель");
+
+        //Можно засунуть объекты классов в массив тоже
+        Car[] carArray = {car1, car2, car4, car3};
+        System.out.println(carArray);
+        // потом переключиться
+        List<Car> carList1 = new ArrayList<>(Arrays.asList(carArray));
+        System.out.println(carList1);
+        }
     }
 
-
-}
